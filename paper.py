836095ad00d188f7a05bd8e8f979872bdef6f862
Benson911214 <95857722+Benson911214@users.noreply.github.com>
2021-12-15 14:53:05 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -8,53 +8,53 @@ computer = random.randint(0,2)
 if user1 == computer:
     if user1==0:
         st.write("你的輸入為：剪刀（0）")
-        st.image(picture 0)
+        streamlit.file_uploader(picture 0)
         st.write("隨機生成數字為：0")
-        st.image(picture 0)
+        streamlit.file_uploader(picture 0)
     elif user1==1:
         st.write("你的輸入為：石頭（1）");
-        st.image(picture 1)
+        streamlit.file_uploader(picture 1)
         st.write("隨機生成數字為：1");
-        st.image(picture 1)
+        streamlit.file_uploader(picture 1)
     else:
         st.write("你的輸入為：布（2）");
-        st.image(picture 2)
+        streamlit.file_uploader(picture 2)
         st.write("隨機生成數字為：2");
-        st.image(picture 2)
+        streamlit.file_uploader(picture 2)
     st.write("啊哈，是平局！");
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
-    st.image(picture 0)
+    streamlit.file_uploader(picture 0)
     st.write("隨機生成數字為：1");
-    st.image(picture 1)
+    streamlit.file_uploader(picture 1)
     st.write(("哈哈，你輸了"));
 elif user1 == 0 and computer == 2:
     st.write("你的輸入為：剪刀（0）");
-    st.image(picture 0)
+    streamlit.file_uploader(picture 0)
     st.write("隨機生成數字為：2");
-    st.image(picture 2)
+    streamlit.file_uploader(picture 2)
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 0:
     st.write("你的輸入為：石頭（1）");
-    st.image(picture 1)
+    streamlit.file_uploader(picture 1)
     st.write("隨機生成數字為：0");
-    st.image(picture 0)
+    streamlit.file_uploader(picture 0)
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 2:
     st.write("你的輸入為：石頭（1）");
-    st.image(picture 1)
+    streamlit.file_uploader(picture 1)
     st.write("隨機生成數字為：2");
-    st.image(picture 2)
+    streamlit.file_uploader(picture 2)
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 0:
     st.write("你的輸入為：布（2）");
-    st.image(picture 2)
+    streamlit.file_uploader(picture 2)
     st.write("隨機生成數字為：0");
-    st.image(picture 0)
+    streamlit.file_uploader(picture 0)
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 1:
     st.write("你的輸入為：布（2）");
-    st.image(picture 2)
+    streamlit.file_uploader(picture 2)
     st.write("隨機生成數字為：1");
-    st.image(picture 1)
+    streamlit.file_uploader(picture 1)
     st.write(("恭喜你贏啦"))
