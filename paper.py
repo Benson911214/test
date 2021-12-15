76c57235b140509846e39f43cc03e129c200ea35
Benson911214 <95857722+Benson911214@users.noreply.github.com>
2021-12-15 14:01:10 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -14,9 +14,9 @@ if user1 == computer:
         ![image]
     else:
         st.write("你的輸入為：布（2）");
-        ![image]
+        ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
         st.write("隨機生成數字為：2");
-        ![image]
+        ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
     st.write("啊哈，是平局！");
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
@@ -28,7 +28,7 @@ elif user1 == 0 and computer == 2:
     st.write("你的輸入為：剪刀（0）");
     ![image]
     st.write("隨機生成數字為：2");
-    ![image]
+    ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 0:
     st.write("你的輸入為：石頭（1）");
@@ -40,17 +40,17 @@ elif user1 == 1 and computer == 2:
     st.write("你的輸入為：石頭（1）");
     ![image]
     st.write("隨機生成數字為：2");
-    ![image]
+    ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 0:
     st.write("你的輸入為：布（2）");
-    ![image]
+    ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
     st.write("隨機生成數字為：0");
     ![image]
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 1:
     st.write("你的輸入為：布（2）");
-    ![image]
+    ![image]https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
     st.write("隨機生成數字為：1");
     ![image]
     st.write(("恭喜你贏啦"))
