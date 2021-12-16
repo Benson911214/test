78ad64665a7fc3f03226cb1101fc7a0daebb25c3
Benson911214 <95857722+Benson911214@users.noreply.github.com>
2021-12-16 20:11:55 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -7,14 +7,14 @@ computer = random.randint(0,2)
 if user1 == computer:
     if user1==0:
         st.write("你的輸入為：剪刀（0）")
-        st.image(scissors.png)
+        st.image(下載 (2).png)
         st.write("隨機生成數字為：0")
-        st.image(scissors.png)
+        st.image(下載 (2).png)
     elif user1==1:
         st.write("你的輸入為：石頭（1）");
-        st.image(stone.png)
+        st.image(下載 (1).png)
         st.write("隨機生成數字為：1");
-        st.image(stone.png)
+        st.image(下載 (1).png)
     else:
         st.write("你的輸入為：布（2）");
         st.image(下載.png)
@@ -23,25 +23,25 @@ if user1 == computer:
     st.write("啊哈，是平局！");
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
-    st.image(scissors.png)
+    st.image(下載 (2).png)
     st.write("隨機生成數字為：1");
-    st.image(stone.png)
+    st.image(下載 (1).png)
     st.write(("哈哈，你輸了"));
 elif user1 == 0 and computer == 2:
     st.write("你的輸入為：剪刀（0）");
-    st.image(scissors.png)
+    st.image(下載 (2).png)
     st.write("隨機生成數字為：2");
     st.image(下載.png)
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 0:
     st.write("你的輸入為：石頭（1）");
-    st.image(stone.png)
+    st.image(下載 (1).png)
     st.write("隨機生成數字為：0");
-    st.image(scissors.png)
+    st.image(下載 (2).png)
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 2:
     st.write("你的輸入為：石頭（1）");
-    st.image(stone.png)
+    st.image(下載 (1).png)
     st.write("隨機生成數字為：2");
     st.image(下載.png)
     st.write(("哈哈，你輸了"));
@@ -49,11 +49,11 @@ elif user1 == 2 and computer == 0:
     st.write("你的輸入為：布（2）");
     st.image(下載.png)
     st.write("隨機生成數字為：0");
-    st.image(scissors.png)
+    st.image(下載 (2).png)
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 1:
     st.write("你的輸入為：布（2）");
     st.image(下載.png)
     st.write("隨機生成數字為：1");
-    st.image(stone.png)
+    st.image(下載 (1).png)
     st.write(("恭喜你贏啦"))
