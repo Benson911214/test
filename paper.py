059fae4d14af60ebe97f789461dd77969eed119b
Benson911214 <95857722+Benson911214@users.noreply.github.com>
2021-12-16 20:10:09 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -2,9 +2,6 @@ import streamlit as st
 user1 = st.number_input("請輸入：剪刀：（0）、石頭（1）、布（2）->")
 confirm_input = st.button('輸入確認')
 if confirm_input:
-scissors.png=https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89.png?raw=true
-stone.png=https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(2).png?raw=true
-paper.png=https://github.com/Benson911214/test/blob/main/%E4%B8%8B%E8%BC%89%20(1).png?raw=true
 import random
 computer = random.randint(0,2)
 if user1 == computer:
@@ -20,9 +17,9 @@ if user1 == computer:
         st.image(stone.png)
     else:
         st.write("你的輸入為：布（2）");
-        st.image(paper.png)
+        st.image(下載.png)
         st.write("隨機生成數字為：2");
-        st.image(paper.png)
+        st.image(下載.png)
     st.write("啊哈，是平局！");
 elif user1 == 0 and computer == 1:
     st.write("你的輸入為：剪刀（0）");
@@ -34,7 +31,7 @@ elif user1 == 0 and computer == 2:
     st.write("你的輸入為：剪刀（0）");
     st.image(scissors.png)
     st.write("隨機生成數字為：2");
-    st.image(paper.png)
+    st.image(下載.png)
     st.write(("恭喜你贏啦！"));
 elif user1 == 1 and computer == 0:
     st.write("你的輸入為：石頭（1）");
@@ -46,17 +43,17 @@ elif user1 == 1 and computer == 2:
     st.write("你的輸入為：石頭（1）");
     st.image(stone.png)
     st.write("隨機生成數字為：2");
-    st.image(paper.png)
+    st.image(下載.png)
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 0:
     st.write("你的輸入為：布（2）");
-    st.image(paper.png)
+    st.image(下載.png)
     st.write("隨機生成數字為：0");
     st.image(scissors.png)
     st.write(("哈哈，你輸了"));
 elif user1 == 2 and computer == 1:
     st.write("你的輸入為：布（2）");
-    st.image(paper.png)
+    st.image(下載.png)
     st.write("隨機生成數字為：1");
     st.image(stone.png)
     st.write(("恭喜你贏啦"))
